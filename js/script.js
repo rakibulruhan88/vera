af4ef51d714b3527adebbda5662b1c8d468e567d
rakibulruhan88 <mdrohan7224@gmail.com>
2024-08-06 17:58:55 +0600

    office work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let menuBtn = document.querySelector(".menu-btn img");
 let closeBtn = document.querySelector(".res-navigation ul li span");
-let resNav = document.querySelector(".hide");
+let resNav = document.querySelector(".res-navigation");
 function responsiveNavigation() {
   menuBtn.addEventListener("click", () => {
-    resNav.style.right = "-13%";
+    resNav.style.right = "0%";
   });
 
   closeBtn.addEventListener("click", () => {
